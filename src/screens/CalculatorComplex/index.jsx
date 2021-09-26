4f6b952fec16967d5ed1572186b1d0e7637811b7
cundus <pcinta0@gmail.com>
2021-09-27 05:39:05 +0700

    ready to build

---
+++ src/screens/CalculatorComplex/index.jsx
@@ -0,0 +1,155 @@
+import { HStack, VStack } from "native-base";
+import React from "react";
+import { View, Text } from "react-native";
+import ButtonReuse from "../../components/ButtonReuse";
+import DisplayCalculator from "../../components/DisplayCalculator";
+
+const CalculatorComplex = ({ navigation }) => {
+  const [currentNumber, setCurrentNumber] = React.useState("");
+  const [lastNumber, setLastNumber] = React.useState("");
+  const [historyNumbers, setHistoryNumbers] = React.useState([]);
+
+  const titleBtn = [
+    {
+      title: ["~", "C", "DEL"],
+    },
+    {
+      title: [1, 2, "-", "+"],
+    },
+    {
+      title: [3, 4, "/", "*"],
+    },
+    {
+      title: [5, 6, "%", "="],
+    },
+    {
+      title: [7, 8, 9, 0],
+    },
+  ];
+
+  const operators = ["+", "-", "*", "/", "%"];
+
+  const handleInput = (btnPressed) => {
+    if (
+      btnPressed === "+" ||
+      btnPressed === "-" ||
+      btnPressed === "*" ||
+      btnPressed === "/" ||
+      btnPressed === "%"
+    ) {
+      let numberArray = currentNumber.split("");
+      let lastIndex = numberArray[numberArray.length - 1];
+      // console.log(lastIndex);
+
+      if (lastIndex === btnPressed) {
+        return currentNumber;
+      } else {
+        if (operators.includes(lastIndex)) {
+          numberArray.pop();
+          numberArray.push(btnPressed);
+          // console.log(numberArray);
+          setCurrentNumber(numberArray.join(""));
+        } else {
+          setCurrentNumber(currentNumber + btnPressed);
+        }
+      }
+
+      return;
+    }
+
+    switch (btnPressed) {
+      case "DEL":
+        setCurrentNumber(currentNumber.substring(0, currentNumber.length - 1));
+        return;
+      case "C":
+        setLastNumber("");
+        setCurrentNumber("");
+        return;
+      case "~":
+        navigation.navigate("History", { historyNumbers });
+        return;
+      case "=":
+        calculate();
+        return;
+    }
+    setCurrentNumber(currentNumber + btnPressed);
+  };
+
+  const calculate = () => {
+    let lastArr = currentNumber[currentNumber.length - 1];
+    // console.log(lastArr);
+    if (
+      lastArr === "/" ||
+      lastArr === "*" ||
+      lastArr === "-" ||
+      lastArr === "+" ||
+      lastArr === "%" ||
+      lastArr === "="
+    ) {
+      return;
+    } else {
+      if (!currentNumber) return;
+      let result = eval(currentNumber).toString();
+      setLastNumber(currentNumber + "=");
+      setCurrentNumber(result);
+
+      setHistoryNumbers((currNumbers) => [
+        ...currNumbers,
+        { calc: currentNumber + "=", resultCalc: result },
+      ]);
+      return;
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <VStack space={4} w="100%" pt={5} px={10}>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+          Calculator
+        </Text>
+        <DisplayCalculator
+          lastNumber={lastNumber}
+          currentNumber={currentNumber}
+        />
+        {titleBtn.map((item, idx) =>
+          item.title.length < 4 ? (
+            <HStack space={4} key={idx} justifyContent="space-between">
+              {item.title.map((value, idx2) => (
+                <ButtonReuse
+                  key={idx2}
+                  onPress={() => handleInput(value)}
+                  colorScheme={
+                    isNaN(parseInt(value)) ? "newIdentityDark" : "newIdentity"
+                  }
+                  size={16}
+                  style={{ width: 100 }}
+                  title={value}
+                />
+              ))}
+            </HStack>
+          ) : (
+            <HStack space={4} key={idx} justifyContent="space-between">
+              {item.title.map((value, idx2) => (
+                <ButtonReuse
+                  key={idx2}
+                  onPress={() => handleInput(value)}
+                  colorScheme={
+                    isNaN(parseInt(value)) ? "newIdentityDark" : "newIdentity"
+                  }
+                  size={16}
+                  title={value}
+                />
+              ))}
+            </HStack>
+          )
+        )}
+      </VStack>
+    </View>
+  );
+};
+
+export default CalculatorComplex;
